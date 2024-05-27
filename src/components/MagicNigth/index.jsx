f67942b14ca32fbc7f34e64e3a38a613de32ba43
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-05-27 16:10:24 -0300

    A

--- src/components/MagicNigth/index.jsx
+++ src/components/MagicNigth/index.jsx
@@ -7,7 +7,7 @@ const Highlight = () => {
             <div>
                 <h1 className="title lg center blackItalic"> PREPAREM-SE PARA UMA NOITE MÁGICA E MUITO ESPECIAL!</h1>
                 <p className={`${styles.paragraph} paragraph center`}>Uma noite de celebração da música brasileira em sua forma mais autêntica e apaixonada. SIM! <span className="bold"> Dia 29/6, na AABB, a partir das 21h</span>, os mestres <span className="bold">JOÃO BOSCO e HAMILTON DE HOLANDA </span>se encontram no contagiante show: EU VOU PRO SAMBA!</p>
-                <button className="btn">EU QUERO IR</button>
+            <Button  text='EU QUERO IR' className={'btn'}/>
             </div>
         </section>
 
